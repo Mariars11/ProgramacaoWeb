dfd18f73dc7c38b73a228d0c1a82835253a19b13
MARIA CLARA RODRIGUES SILVA <mariaclara.rodrigues@a.ucb.br>
2023-08-10 11:14:25 -0300

    Codigo realizado em sala

---
+++ 10-08-23/exercicio01.js
@@ -0,0 +1,14 @@
+let numA = Number(25);
+let numB = Number(5);
+
+let soma = numA + numB;
+console.log("Resultado da soma entre " + numA + " e " + numB + " é " + soma);
+
+let subtracao = numA - numB;
+console.log("Resultado da subtração entre " + numA + " e " + numB + " é " + subtracao);
+
+let multiplicacao = numA * numB;
+console.log("Resultado da multiplicação entre " + numA + " e " + numB + " é " + multiplicacao);
+
+let divisao = numA / numB;
+console.log("Resultado da divisão entre " + numA + " e " + numB + " é " + divisao);
---
+++ 10-08-23/main.js
@@ -0,0 +1,46 @@
+const n = 1; //constante
+console.log(n);
+let l = 2; //permitir
+var v = 3; 
+let bloco = "oi";
+
+if(n == 1){
+    const pi = 3.14;
+    console.log(pi);
+    bloco = "olá"
+}
+//console.log(nome);
+let nome = "xpto";
+//nome_var = "oi";
+console.log(nome_var);
+var nome_var = "xpto";
+
+//console.log(pi);
+console.log(bloco);
+
+let texto = String("Meu Texto");
+console.log(texto.charAt(2));
+
+let numero = Number(1);
+console.log(numero);
+
+numero = Number(2.32654);
+console.log(numero);
+
+numero = BigInt(2456487987456456465213212645465);
+console.log(numero);
+
+let bool = Boolean(true);
+console.log(bool);
+
+let nulo = null;
+console.log(nulo);
+
+let indefinido = undefined;
+console.log(indefinido);
+
+console.log(1+1);
+console.log(1-1);
+console.log(1/1);
+console.log(1*1);
+
