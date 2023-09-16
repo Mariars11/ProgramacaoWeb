b140ae7c156f4656af120aa9b6b8e9d6dd9f80fe
Maria Clara <maria.clara@bcb.gov.br>
2023-09-16 18:52:10 -0300

    Adicionando comentarios no desafio 1

--- 31-08-23 - Aula 4/Desafios/desafio1.js
+++ 31-08-23 - Aula 4/Desafios/desafio1.js
@@ -1,34 +1,33 @@
-const prompt = require("prompt-sync")();
-/*
-Utilizando o prompt, implemente uma função recursiva que 
-receba 5 números, separados por vírgula, e printe-os no 
-console. 
-O critério de saída da função recursiva, será a correta execução 
-do programa. Enquanto os 5 valores inseridos não forem 
-numéricos, ou houverem menos/mais números e posições. É 
-importante mostrar quais destes valores inseridos não são 
-numéricos.
- */
+const prompt = require("prompt-sync")(); //possibilita o uso do prompt
+
 const Numeros = {
-    hasNaN: function(array = Array()) {
+    //Tratativa para verificar se não é um numero
+    hasNaN: function(array = Array()) //Recebe o array e analisa todos os elementos
+    { 
         isNaN = false;
-        for(let i = 0; i < array.length; i++){
+
+        for(let i = 0; i < array.length; i++)
+        {
             let number = Number(array[i]);
 
-            if(Number.isNaN(number)){
+            if(Number.isNaN(number))
+            {
                 isNaN = true;
                 console.log(`${array[i]} não é um número. Digite novamente!`)
             }
         }
         return isNaN;
     },
-    printArray: function(array = Array()) {
-        for(let i = 0; i < array.length; i++){
+    printArray: function(array = Array()) 
+    {
+        for(let i = 0; i < array.length; i++)
+        {
             console.log(`${array[i]}`);
         }
         
     },
-    funcaoRecursiva: function (){
+    funcaoRecursiva: function ()
+    {
         let numeros = Array();
         numeros = prompt("Informe 5 números (separe os por virgula na mesma linha): ").split(",");
         let isNaN = this.hasNaN(numeros);
