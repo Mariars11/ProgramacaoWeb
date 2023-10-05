7aa5b426c46955f35730ad4224e4360d96951a03
Maria Clara <maria.clara@bcb.gov.br>
2023-10-05 10:44:24 -0300

    Adicionando material da aula 8

---
+++ 05-10-23 - Aula 8/index.js
@@ -0,0 +1,62 @@
+//npm install --save body-parser
+//npm install express@4.18.2 
+//npm install mustache-express@1.3.2
+
+const express = require('express');
+const mustacheExpress = require('mustache-express');
+
+const app = express();
+
+const PORT = 8000;
+//renderizador - motor
+app.engine("html", mustacheExpress()); //html - nome usado; mustacheExpress() - construtor 
+var bodyParser = require('body-parser')
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+app.use(express.static(__dirname + "/public"));
+app.set("view engine", "html"); //view engine é proprio do engine
+//Utiliza a pasta Views como padrao, caso seja usado um outro nome precisa dessa declaração
+app.set("views", __dirname + "/views"); //views - não é o nome da pasta, é uma propriedade
+
+app.get('/', function (req, res) {
+    res.render("index.html");
+});
+
+let dados = {
+    nome: String(),
+    telefone: String(),
+    endereco: String(),
+    data: Date(),
+}
+
+app.post('/dados', function(req, res) {
+    dados.nome = req.body.nome;
+    dados.telefone = req.body.telefone;
+    dados.endereco = req.body.endereco;
+    dados.data = req.body.dataAgendamento;
+
+    res.redirect("/dados");
+});
+
+app.get('/dados', function(req, res) {
+    let erro_form = false;
+    if(dados.nome == ""){
+        erro_form = true;
+    }
+    if(dados.data == ""){
+        erro_form = true;
+    }
+    if(dados.endereco == ""){
+        erro_form = true;
+    }
+    if(dados.telefone == ""){
+        erro_form = true;
+    }
+    res.render("dados.html", {dados, erro_form});
+});
+
+app.listen(PORT, function(){
+    console.log(`App rodando na porta ${PORT}`)
+});
